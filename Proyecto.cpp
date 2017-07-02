8bbf042d2dc7b16608267ea8c7578173f667f0c6
Marcello Defilippi <marcello.defilippi@utec.edu.pe>
2017-07-02 00:56:59 +0000

    Segundo avance

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <sstream>
 #include <list>
+#include <map>
 
 using namespace std;
 
@@ -19,6 +20,7 @@ public :
 	float y;
 	float distancia;
 	string estado;
+	string accion;
 };
 
 // Clase contenedor
@@ -42,7 +44,8 @@ public:
 		this->nombreArchivo = nombreArchivo;
 	}
 
-	void obtenerData(string csv){
+	void obtenerData(string csv)
+	{
 
 		ifstream file (csv); // declare file stream: http://www.cplusplus.com/reference/iostream/ifstream/		
 
@@ -51,13 +54,15 @@ public:
 		string identificador, posX, posY, Estado;
 
 		if (!file.fail()){
-			while(!file.eof()){			
+			while(!file.eof())
+			{			
 				getline(file, identificador,';');			
 				getline(file, posX,';');
 				getline(file, posY,';');
 				getline(file, Estado,file.widen('\n'));
 
-				if (identificador != ""){
+				if (identificador != "")
+				{
 					TPunto punto = {atoi(identificador.c_str()), identificador.c_str(), stof(posX), stof(posY), 0, Estado};
 					listaPuntos.push_back(punto);			
 				}
@@ -66,7 +71,6 @@ public:
 
 	}
 
-	// Metodos de Procesamiento
 	void agregarPunto(string nombre,float x, float y, string estado)
 	{	
 		int idx = listaPuntos.size() + 1;
@@ -86,8 +90,10 @@ public:
 		grabarArchivo();
 	}
 
-	void armarTabla(vector<TPunto> listaPuntos, int cantidad){
-		if (listaPuntos.size() == 0){
+	void armarTabla(vector<TPunto> listaPuntos, int cantidad)
+	{
+		if (listaPuntos.size() == 0)
+		{
 			cout << "No hay registros para mostrar.\n\n";
 			return;
 		}
@@ -114,7 +120,8 @@ public:
 
 	void listarPuntosAlrededor(float x, float y)
 	{
-		if (listaPuntos.size() == 0){
+		if (listaPuntos.size() == 0)
+		{
 			cout << "No hay registros para mostrar.\n\n";
 			return;
 		}
@@ -129,21 +136,25 @@ public:
 
 			listaPuntos[i].distancia = distancia;
 
-			if (distancia < 15){
-				puntoAlrededor = puntoAlrededor + listaPuntos[i].nombre;
+			if (distancia < 15)
+			{
+				puntoAlrededor = puntoAlrededor + " " + listaPuntos[i].nombre;
 			}
 		}
 
-		if (puntoAlrededor == ""){
-			cout << "No hay puntos cerccanos " << endl;			
+		if (puntoAlrededor == "")
+		{
+			cout << "No hay puntos cercanos " << endl;			
 			return;
 		}
 
-		cout << "Los puntos mas cercanos son " << puntoAlrededor << endl;			
+		cout << "Los puntos mas cercanos son: " << puntoAlrededor << endl;			
 	}
 
-	void hallarDistancia(){
-		if (listaPuntos.size() == 0){
+	void hallarDistancia()
+	{
+		if (listaPuntos.size() == 0)
+		{
 			cout << "No hay registros para mostrar.\n\n";
 			return;
 		}
@@ -155,7 +166,8 @@ public:
 			double x = 0;
 			double y = 0;
 
-			if (listaPuntos.size() <= i){
+			if (listaPuntos.size() <= i)
+			{
 				x = listaPuntos[i + 1].x;
 				y = listaPuntos[i + 1].y;
 			}
@@ -172,41 +184,56 @@ public:
 		cout << "La distancia promedio es de " << totalDistancia / (listaPuntos.size() - 1)  << endl;			
 	}
 
-	void distanciaTodos(){
-		if (listaPuntos.size() == 0){
+	void distanciaTodos(string accion)
+	{
+		if (listaPuntos.size() == 0)
+		{
 			cout << "No hay registros para mostrar.\n\n";
 			return;
 		}
-
-		string puntoAlrededor = "";
-		double totalDistancia = 0;
+		
+		vector <double> DISTANCIAS;
+		double totalDistancia=0;
 		int totalPuntos = listaPuntos.size() - 1;
-		for (int i = 0; i<=totalPuntos; i++)
+		for (int i = 0; i <= totalPuntos; i++)
 		{
-			double xIni = listaPuntos[i].x;
-			double yIni = listaPuntos[i].y;
-
+			double xinicial = listaPuntos[i].x;
+			double yinicial = listaPuntos[i].y;
 			double x = 0;
 			double y = 0;
-			if (totalPuntos > i){
+			
+			if (totalPuntos > 1)
+			{
 				x = listaPuntos[i + 1].x;
 				y = listaPuntos[i + 1].y;
 			}
+			
+			double distanciaX = xinicial - x;
+			double distanciaY = yinicial - y;
 
-			double x_diff = xIni - x;
-			double y_diff = yIni - y;
-
-			double distancia = sqrt(x_diff * x_diff + y_diff * y_diff); 
+			double distancia = sqrt(distanciaX * distanciaX + distanciaY * distanciaY); 
 			totalDistancia =+ distancia;
 
 			listaPuntos[i].distancia = distancia;
-
-			if (totalPuntos > i){
-				cout << "La distancia del " << listaPuntos[i].nombre << " es " << distancia << endl;			
-			}
+			DISTANCIAS.push_back(distancia);
 		}
-
-		cout << "La distancia del " << listaPuntos[totalPuntos].nombre << " es " << totalDistancia << endl;
+		double mayor = 0;
+		double menor = 0;
+		for (int i = 0; i < DISTANCIAS.size(); i++)
+		{
+			if (DISTANCIAS[i]>mayor){
+				mayor = DISTANCIAS[i];}
+		}
+		for (int i = 0; i < DISTANCIAS.size(); i--)
+		{
+			if (DISTANCIAS[i]<menor){
+				menor = DISTANCIAS[i];}
+		}
+		if (accion == "mayor")
+			cout << "La mayor distancia entre puntos es: " << mayor << endl;
+		if (accion == "menor")
+			cout << "La menor distancia entre puntos es: "  << menor << endl;
+		
 	}
 
 	void listarPuntosAbiertos()
@@ -217,7 +244,8 @@ public:
 			double x = 0;
 			double y = 0;
 
-			if (listaPuntos.size() <= i){
+			if (listaPuntos.size() <= i)
+			{
 				x = listaPuntos[i + 1].x;
 				y = listaPuntos[i + 1].y;
 			}
@@ -248,12 +276,13 @@ public:
 		armarTabla(listaPuntosA, 100);
 	}
 
-	void listarPuntosFiltro(std::string nombre)
+	void listarPuntosFiltro(string nombre)
 	{
 		vector<TPunto> listaPuntosFiltro;
 		for (int i = 0; i<=listaPuntos.size() - 1; i++)
 		{
-			if (listaPuntos[i].nombre == nombre){
+			if (listaPuntos[i].nombre == nombre)
+			{
 				listaPuntosFiltro.push_back(listaPuntos[i]);
 			}
 		}
@@ -270,6 +299,39 @@ public:
 			archivo << punto.nombre << ";" << punto.x << ";" << punto.y << ";" << punto.estado << endl;
 		archivo.close();
 	}
+	void ClearScreen()
+    {
+    cout << string( 100, '\n' );
+    }
+};
+struct TCajero
+{
+   string banco;
+   string dir1;
+   string dir2;
+   string dir3;
+};
+class CCajero
+{
+	public:
+	void hallarCajero(string banco, float a, float b)
+	{
+		float c = sqrt(a*a+b*b); // Distancia a la redonda
+		map<string, TCajero> map;
+		map.insert(pair<string, TCajero>("BCP",{"BCP", "Av.Grau 123", "Calle Los Cisnes 342", "Av.Almendáriz 1189"}));
+		map.insert(pair<string, TCajero>("BBVA",{"BBVA", "Av.Grau 246", "Calle Cerro Gris 978", "Av. Conquistadores 3267"}));
+		map.insert(pair<string, TCajero>("SCOTIABANK",{"SCOTIABANK", "Av.Uno 354", "Calle Lima 221", "Av. Ayacucho 657"}));
+		 
+		string busqueda;
+		cout << "Ingrese el nombre del Banco: ";
+		cin >> busqueda;
+		auto dato=map[busqueda];
+		system ("clear");
+		cout << "El cajero " << dato.banco << " más cercano está en: " << endl;
+		if (c<1000 && c>99) cout << dato.dir3;
+		if (c<100 && c>9) cout << dato.dir2;
+		if (c<10) cout << dato.dir1;
+	}
 };
 
 // Metodo Menu
@@ -279,7 +341,8 @@ int menu()
 
 	do
 	{	
-			system("clear");
+		system("clear");
+		void ClearScreen();
 		cout << "MENU" << endl;
 		cout << string(4, '~') << endl << endl;
 
@@ -288,8 +351,8 @@ int menu()
 		cout << "3. Listar 100 primeros puntos." << endl;
 		cout << "4. Listar los puntos ubicados alrededor." << endl;
 		cout << "5. Hallar la distancia de cada punto y obtener el promedio." << endl;
-		cout << "6. Hallar la mayor distancia entre los puntos. " << endl;
-		cout << "7. Hallar la menor distancia entre los puntos." << endl;
+		cout << "6. Hallar la mayor o menor distancia entre los puntos. " << endl;
+		cout << "7. Hallar el cajero más cercano." << endl;
 	    cout << "8. Listar los puntos con estado abierto a menos de 100 metros." << endl;
 		cout << "9. Filtrar ubicacion." << endl;
 		cout << "0. Para salir." << endl;
@@ -308,10 +371,12 @@ int main()
 	cout << "\n\n";
 	string nombre;
 	string estado;
+	string accion;
+	float a; float b; string banco;
 
 	string nombreCSV = ciudad + ".CSV";
 	CMapa mapa(ciudad, nombreCSV);       // Mapa de Lima
-
+	CCajero cajero;
 	mapa.obtenerData(nombreCSV);
 
 	int opcion = 0;
@@ -391,15 +456,21 @@ int main()
 				cin >> nombre;		
 			}
 			mapa.eliminarPuntoPorIdentificador(nombre);
+			cout << "El punto ha sido eliminado de su mapa";
 			cin.ignore(numeric_limits<streamsize>::max(),'\n');
 			cin.get();
+		
 			break;
+			
 		case 3: 
 		system("clear");
 			mapa.listarCienPuntos(100);
 			cin.ignore(numeric_limits<streamsize>::max(),'\n');
 			cin.get();
+			void ClearScreen();
+			cout << "\033[2J\033[1;1H";
 			break;
+
 		case 4:	
 		system("clear");
 			cout << "Ingrese posicion X: ";
@@ -450,13 +521,16 @@ int main()
 			break;
 		case 6: 
 		system("clear");
-			mapa.distanciaTodos();
+			cout << "Desea hallar la menor o mayor distancia? "; cin >> accion;
+			mapa.distanciaTodos(accion);
 			cin.ignore(numeric_limits<streamsize>::max(),'\n');
 			cin.get();
 			break;
 		case 7: 
 		system("clear");
-			mapa.distanciaTodos();
+			cout << "Ingrese su posicion en X: "; cin >> a;
+			cout << "Ingrese su posicion en Y :"; cin >> b;
+			cajero.hallarCajero(nombre, a, b);
 			cin.ignore(numeric_limits<streamsize>::max(),'\n');
 			cin.get();
 			break;
